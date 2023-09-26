3fa7dfe734706d83dbdd77c82cb0ba6ccc33bbe5
trupti.d <trupti.d@MYSTILAP-322.mystifly.net>
2023-09-26 21:18:20 +0530

    Even number code

---
+++ src/Truptidemo1/Evennum.java
@@ -0,0 +1,14 @@
+package Truptidemo1;
+
+public class Evennum {
+	
+		public static void main(String[] args) {
+	        int num=19;
+	        if (num % 2 == 0) {
+	            System.out.println(" is even.");
+	        } else {
+	            System.out.println(" is odd.");
+	        }
+	        
+		}
+}
--- src/Truptidemo1/Hello_world.java
+++ src/Truptidemo1/Hello_world.java
@@ -4,6 +4,7 @@ public class Hello_world {
 	public static void main(String args[])
 	{
 		System.out.println("Hii world");
+		
 	}
 
 }
